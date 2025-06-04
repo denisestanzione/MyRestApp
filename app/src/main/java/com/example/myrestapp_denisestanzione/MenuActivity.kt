76d95f8a055b307fc564d2eabcddfa69dbb343ae
denisestanzione <denisestanzione06@gmail.com>
2025-06-04 18:09:34 +0200

    Creata l'activity per la web view del sito di netflix e l'intent per il passaggio ad essa

--- app/src/main/java/com/example/myrestapp_denisestanzione/MenuActivity.kt
+++ app/src/main/java/com/example/myrestapp_denisestanzione/MenuActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myrestapp_denisestanzione
 
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +15,19 @@ class MenuActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
 
+        val btnWebView = findViewById<ImageButton>(R.id.btnWebView)
+        val btnSearch = findViewById<ImageButton>(R.id.btnSearch)
+        val btnDashboard = findViewById<ImageButton>(R.id.btnDashboard)
+
+        // Al click del pulsante per la web view di netflix
+        btnWebView.setOnClickListener {
+            intentWebView()
+        }
+
+    }
+
+    private fun intentWebView(){
+        val mioIntent = Intent(this, WebViewActivity::class.java)
+        startActivity(mioIntent)
     }
 }
